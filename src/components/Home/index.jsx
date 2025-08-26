b98ba1d3f70ebe79977a799f6a9e8a4be90182ac
Arthur Weber <darkw@PC-Weber>
2025-08-26 15:11:02 -0300

    Versão definitiva de Home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -6,7 +6,15 @@ function Home({ username, setUsername }) {
         <div className={styles.home}>
             <h1 className={styles.home__title}>Bem-vindo à Calculadora de IMC</h1>
             <p className={styles.textInfo}>
-                O Índice de Massa Corporal (IMC) é uma métrica usada para avaliar...
+                O Índice de Massa Corporal (IMC) é uma métrica usada para avaliar
+                se o peso de uma pessoa está adequado em relação à sua altura,
+                ajudando a estimar a quantidade de gordura corporal e identificar
+                possíveis riscos à saúde. Profissionais da saúde utilizam o IMC
+                para classificar o peso e avaliar o risco de doenças como obesidade,
+                desnutrição, diabetes tipo 2 e hipertensão. No entanto, ele não
+                diferencia massa muscular de gordura, podendo não refletir com
+                precisão a composição corporal de atletas ou pessoas com alta
+                musculatura.
             </p>
             <h3 className={styles.subtitle}>
                 Para garantir que sua saúde está em dia,
