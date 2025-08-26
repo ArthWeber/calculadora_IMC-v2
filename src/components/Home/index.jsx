be157863a0191ffa3db1c058a95893d1af2d5107
Arthur Weber <darkw@PC-Weber>
2025-08-26 14:58:43 -0300

    Home modificado

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,9 +4,9 @@ import './Home.module.css';
 function Home({ username, setUsername }) {
 
     return (
-        <div className="styles.home">
-            <h1 className="styles.home__title">Bem-vindo à Calculadora de IMC</h1>
-            <p className="styles.home__textInfo">O Índice de Massa Corporal (IMC) é uma métrica usada para avaliar
+        <div className="home">
+            <h1 className="home__title">Bem-vindo à Calculadora de IMC</h1>
+            <p className="textInfo">O Índice de Massa Corporal (IMC) é uma métrica usada para avaliar
                 se o peso de uma pessoa está adequado em relação à sua altura,
                 ajudando a estimar a quantidade de gordura corporal e identificar
                 possíveis riscos à saúde. Profissionais da saúde utilizam o IMC
@@ -16,11 +16,11 @@ function Home({ username, setUsername }) {
                 precisão a composição corporal de atletas ou pessoas com alta
                 musculatura.
             </p>
-            <h3 className="styles.subtitle"> Para garantir que sua saúde está em dia,
+            <h3 className="subtitle"> Para garantir que sua saúde está em dia,
                 <span> sempre busque ajuda com um profissional da área.</span>
             </h3>
 
-            <p className="styles.textInfo">Para começar, por favor, digite seu nome:</p>
+            <p className="textInfo">Para começar, por favor, digite seu nome:</p>
             <input
                 type="text"
                 value={username}
